e39fd4a64a83a13ca773962c4e76d6ce4f323b36
Rossi <yoandresmza@hotmail.com>
2019-09-10 10:10:02 -0300

    segunda version

--- ej1.py
+++ ej1.py
@@ -6,4 +6,8 @@ def body(url):
     article = Article(url)
     article.download()
     article.parse()
-    return Counter(re.split(r'[\W\d]+',article.text))
+    return Counter(filter(None,re.split(r'[\W\d]+',article.text)))
+    #Counter() devuelve un diccionario
+    #filter(None,...) elimina de la lista las entradas vacias
+    #re.split() devuelve una lista de palabras que para separarlas del todo el texto usa una regular expression como criterio
+    #r'[\W\d] regular expression para separar palabras, \W caracteres que no son una palabra \d numeros y "+" es para que el criterio se aplique sobre caracteres consecutivos
